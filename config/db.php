ea7e244c7a69ac3eee0271841b0c387f1278d335
Laboratorynotices <muenchen.webas@gmail.com>
2023-10-31 07:47:50 +0100

    feat: Подключение к базе данных
    Задаём настройки для подключения к базе данных

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
-    'username' => 'root',
-    'password' => '',
+    'dsn' => 'mysql:host=localhost;dbname=lif-export',
+    'username' => 'lif-export',
+    'password' => 'secretPassword',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
